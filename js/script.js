f9c70709476671e99c89dcc4ce7a2b0bb9471130
Paul Yoder <paulyoder@gmail.com>
2013-01-24 18:28:50 -0600

    updating notes

--- js/script.js
+++ js/script.js
@@ -1,17 +1,1 @@
-var findLastName = function(firstName, callback) {
-  var lastName = 'Yoder';
-  callback(lastName); 
-}
-
-var Person = function(name) {
-  this.firstName = name;
-  this.sayYourName = function() {
-    var self = this;
-    findLastName(this.firstName, function(lastName) {
-      log('my name is ' + self.firstName + ' ' + lastName);
-    });
-  };
-}
-
-var me = new Person('Paul');
-me.sayYourName();
+log('JavaScript is Not C#');
